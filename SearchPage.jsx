c37de5823395f6c087fca7a3c3250f6ec38cf62a
Archit301 <114588710+Archit301@users.noreply.github.com>
2024-07-14 04:01:35 +0530

    Create SearchPage.jsx

---
+++ SearchPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SearchPage = () => {
+  // Implement search results display
+  return <div className="search-page">Search Results</div>;
+};
+
+export default SearchPage;
