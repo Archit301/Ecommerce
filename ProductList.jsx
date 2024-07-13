604a7ca3392cac370053141ed93eca4c2da676cf
Archit301 <114588710+Archit301@users.noreply.github.com>
2024-07-14 03:51:00 +0530

    Create ProductList.jsx

---
+++ ProductList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ProductList = ({ products }) => {
+  return (
+    <div className="product-list">
+      {products.map((product) => (
+        <div key={product._id} className="product-item">
+          <Link to={`/product/${product._id}`}>
+            <img src={product.image} alt={product.name} />
+            <h3>{product.name}</h3>
+            <p>${product.price}</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
