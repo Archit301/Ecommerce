177e5fd5f9730f3f73d4179db88316b6206c3019
Archit301 <114588710+Archit301@users.noreply.github.com>
2024-07-14 03:42:33 +0530

    Create store.jsx

---
+++ store.jsx
@@ -0,0 +1,29 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { productListReducer, productDetailReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailReducer,
+  cart: cartReducer,
+});
+
+const initialState = {
+  cart: {
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
+  },
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
