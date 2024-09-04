2e7fe825ad800173e21566313f24d7c1317031fc
Archit301 <114588710+Archit301@users.noreply.github.com>
2024-09-04 23:11:53 +0530

    Delete SearchPage.jsx

--- SearchPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const SearchPage = () => {
-  // Implement search results display
-  return <div className="search-page">Search Results</div>;
-};
-
-export default SearchPage;
